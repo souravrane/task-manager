ba0c94ec10e70a5ea6f2e1a23c6781252542b3b5
souravrane <sourav1121997@gmail.com>
2022-01-31 20:51:20 +0530

    established connection with db and created a dbname

---
+++ mongodb.js
@@ -0,0 +1,16 @@
+// CRUD operations
+
+const { MongoClient, ObjectID } = require("mongodb");
+
+const connectionURL = "mongodb://127.0.0.1:27017";
+const databaseName = "task-manger";
+
+MongoClient.connect(connectionURL, { useNewParser: true }, (error, client) => {
+    if (error) {
+        return console.log("Unable to connect to database");
+    }
+
+    // gives us a db reference to our db name.
+    const db = client.db(databaseName);
+});
+3;
