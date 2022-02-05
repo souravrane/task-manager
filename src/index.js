3279bc0edfe2434e212b03cbb2e6625bcb008cd0
souravrane <sourav1121997@gmail.com>
2022-02-05 23:57:14 +0530

    created basic user and task apis

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 // we leave it open this way to ensure mongoose runs automatically
 require("./db/mongoose");
 const User = require("./models/user");
+const Task = require("./models/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,11 +14,73 @@ app.post("/users", (req, res) => {
     const user = new User(req.body);
     user.save()
         .then((result) => {
-            res.send(result);
+            res.status(201).send(result);
         })
         .catch((error) => {
             // status setting must be done before send
             res.status(400).send(error);
+        });
+});
+
+app.get("/users", (req, res) => {
+    User.find({})
+        .then((users) => {
+            res.send(users);
+        })
+        .catch((error) => {
+            res.status(500).send();
+        });
+});
+
+app.get("/users/:id", (req, res) => {
+    const _id = req.params.id;
+    User.findById(_id)
+        .then((user) => {
+            if (!user) {
+                return res.status(404).send();
+            }
+            console.log("foudn....");
+
+            res.status(201).send(user);
+        })
+        .catch((error) => {
+            res.status(500).send(error);
+        });
+});
+
+app.post("/tasks", (req, res) => {
+    const task = new Task(req.body);
+    task.save()
+        .then((result) => {
+            res.status(201).send(result);
+        })
+        .catch((error) => {
+            // status setting must be done before send
+            res.status(400).send();
+        });
+});
+
+app.get("/tasks", (req, res) => {
+    Task.find({})
+        .then((tasks) => {
+            res.send(tasks);
+        })
+        .catch((error) => {
+            res.status(500).send();
+        });
+});
+
+app.get("/tasks/:id", (req, res) => {
+    const _id = req.params.id;
+    Task.findById(_id)
+        .then((task) => {
+            if (!task) {
+                return res.status(404).send();
+            }
+            res.send(task);
+        })
+        .catch((error) => {
+            res.status(500).send(error);
         });
 });
 
