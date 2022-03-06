db379f0d4bba40171d0c39e147fd7d6b76719561
souravrane <sourav1121997@gmail.com>
2022-03-06 09:42:24 +0530

    created middleware for authentication at user profile route

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ const taskRouter = require("./routes/task");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+//     if (req.method == "GET") {
+//         res.send("GET requests are disabled");
+//     } else {
+//         next();
+//     }
+// });
+
+// app.use((req, res, next) => {
+//     res.status(503).send("Under maintainance...");
+// });
+
 // this parses the request to json
 app.use(express.json());
 app.use(userRouter);
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const User = require("../models/user");
+const auth = require("../middleware/auth");
+
 const router = new express.Router();
 
 router.post("/users", async (req, res) => {
     const user = new User(req.body);
     try {
         await user.save();
-        res.status(201).send(user);
+        const token = await user.generateAuthToken();
+        res.status(201).send({ user, token });
     } catch (e) {
         res.status(400).send(e);
     }
@@ -20,7 +23,6 @@ router.post("/users/login", async (req, res) => {
             req.body.password
         );
         const token = await user.generateAuthToken();
-
         res.send({ user, token });
     } catch (e) {
         console.log(e);
@@ -28,13 +30,9 @@ router.post("/users/login", async (req, res) => {
     }
 });
 
-router.get("/users", async (req, res) => {
-    try {
-        const users = await User.find({});
-        res.send(users);
-    } catch (e) {
-        res.status(500).send();
-    }
+// adding a middleware to a specific route
+router.get("/users/me", auth, async (req, res) => {
+    res.send(req.user);
 });
 
 router.get("/users/:id", async (req, res) => {
